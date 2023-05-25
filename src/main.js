46b1af7bdc82053c70c50fbf7e13a22a3010f665
Rodrigo Beckmann <rodrigodsbeckmann@gmail.com>
2023-05-25 12:05:18 -0300

    implementando relogio e data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './style.css';
+import moment from 'moment';
+
+const clock = document.querySelector('#actual-time');
+
+
+
+console.log(clock.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
